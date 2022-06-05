09ebe2b69b4884d9b9a4cf31fa45e41fdedf51af
NagaVenkataSaiM <2000030558@kluniversity.in>
2022-06-05 22:41:51 +0530

    added css and fonts

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,8 @@ import rectangle2 from "./images/Rectangle_445.jpg"
 import homedes from "./images/homedes.png"
 import logo from "./images/zonzo-logo.png";
 import bbs from "./images/942.png"
-
+import './CSS/zonzoall.css'
+import './CSS/master-v32.css'
 
 
 function Home(){
@@ -13,6 +14,7 @@ function Home(){
 
 	return (
 		<div style={{ backgroundImage:`url(${bbs})`,height:"100vh"}}>
+		
 		
 		<div class="h-backcont" style={{backgroundImage: `url(${rectangle2})`,backgroundRepeat:"no-repeat"}}>
         <div class="h2-backcont" style={{backgroundImage: `url(${rectangle})`,backgroundRepeat:"no-repeat", opacity:"0.1"}}>
@@ -27,7 +29,7 @@ function Home(){
         
         </div>
         </div>
-
+ <div class="sub-header p-fixed site-white-bk w-100-p z-index-500 border-box border-0 border-btm-1 brk-700-border-top-1 border-solid" > <div class="site-width site-margin mglr-auto"> <div class="flex row brk-700-justify-center"> <div class="flex col justify-center"> <h1 class="font-w-700 font-14 brk-700-font-12">CHANNEL MANAGER</h1> </div> <div class="flex col justify-center"> <div class="ht-50 wd-1 mgl-20 mgr-20 brk-700-mgl-10 brk-700-mgr-10" ></div> </div> <div class="hd-overview header-select flex col justify-center" value="pos-overview"> <span class="font-14 brk-700-font-13 mgr-10 brk-700-mgr-5 header-select-current">Overview</span> </div> <div class="hd-channels header-select flex col justify-center" value="pos-channels"> <span class="font-14 brk-700-font-13 mgl-10 mgr-10 brk-700-mgl-5 brk-700-mgr-5">Channels</span> </div> <div class="hd-features header-select flex col justify-center" value="pos-features"> <span class="font-14 brk-700-font-13 mgl-10 brk-700-mgl-5 brk-700-mgr-20">Features</span> </div> </div> </div> </div>
         <div class="navcont">
 		<img src={logo} class="img1"/>
 		<div class="navcont2">
